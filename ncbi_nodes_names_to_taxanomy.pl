7d1a12a454ba74eb0848c25d1500acaec1bdc96b
First_sight_407 <31948527+wangpengnovos@users.noreply.github.com>
2019-10-17 12:17:21 +0800

    Update ncbi_nodes_names_to_taxanomy.pl

--- ncbi_nodes_names_to_taxanomy.pl
+++ ncbi_nodes_names_to_taxanomy.pl
@@ -7,6 +7,7 @@ my ($nodefiles, $namefiles) = @ARGV;
 
 taxonomy_connection($nodefiles, $namefiles);
 
+############################################
 sub taxonomy_connection{
 
 	my ($node_file, $name_file) = @_;
@@ -14,11 +15,11 @@ sub taxonomy_connection{
 	my (%taxa_parent_taxa, %taxa_level, @taxaids);
 
 	open IN, $node_file || die $!;
-	
+
 	while(<IN>){
 		chomp;
 		my ($taxa_id, $parent_taxa_id, $level) = (split /\t/)[0, 2, 4];
-		
+
 		$taxa_parent_taxa{$taxa_id} = $parent_taxa_id;
 
 		$taxa_level{$taxa_id} = $level;
@@ -26,13 +27,13 @@ sub taxonomy_connection{
 		push @taxaids, $taxa_id;
 	}
 	close IN;
-	
+
 	my %taxainfo = get_scientific_name($name_file);
 
 	for(@taxaids){
-		
+
 		my $info = bottom_to_up_find($_, \%taxa_parent_taxa, \%taxa_level, \%taxainfo);
-		
+
 		my $scientific_info = extract_taxa_info($info);
 		print $_, "\t", $scientific_info, "\t", $info, "\n";
 	}
@@ -44,67 +45,67 @@ sub bottom_to_up_find{
 	my ($id, $taxa2parent, $taxa2level, $taxainfo) = @_;
 
 	my $name = $taxainfo->{$id} ? $taxainfo->{$id} : 'Unclassified';
-	
+
 	my $taxa_chain = $taxa2level->{$id} . '__' . $id . "($name)";
 
 	if($id == 1){
-		
+
 		return $taxa_chain;
-		
+
 	}else{
-		
-		return bottom_to_up_find($taxa2parent->{$id}, $taxa2parent, $taxa2level, $taxainfo) . ';' . $taxa_chain; 
-	
+
+		return bottom_to_up_find($taxa2parent->{$id}, $taxa2parent, $taxa2level, $taxainfo) . ';' . $taxa_chain;
+
 	}
 }
 
 
 sub get_scientific_name{
-	
+
 	my ($names) = @_;
-	
+
 	my %taxaid_name;
-	
+
 	open IN, $names || die $!;
-	
+
 	while(<IN>){
 		chomp;
-		
+
 		(/scientific name/) || next;
-		
+
 		my ($taxa_id, $taxainfo) = (split /\t/)[0, 2];
-		
+		$taxainfo =~ s/;//g; $taxainfo =~ s/__/_/g;
 		$taxaid_name{$taxa_id} = $taxainfo;
 	}
 	close IN;
-	
+
 	return %taxaid_name;
-	
+
 }
 
 
 sub extract_taxa_info{
-	
+
 	my ($fulltaxainfo)  = @_;
-	
+
 	my @target_level = qw/superkingdom phylum class order family genus species subspecies/;
 	my @brief_level = qw/k p c o f g s subs/;
-	#no rank__1(root);no rank__131567(cellular organisms);superkingdom__2(Bacteria);
-	#phylum__203691(Spirochaetes);class__203692(Spirochaetia);order__136(Spirochaetales);
-	#family__137(Spirochaetaceae);genus__157(Treponema);
-	#species__160(Treponema pallidum);subspecies__161(Treponema pallidum subsp. pallidum)
-	
+	#no rank__1(root);no rank__131567(cellular organisms);superkingdom__2(Bacteria);no rank__1783257(PVC group);phylum__67812(Candidatus Omnitrophica);no rank__1047005(unclassified Candidatus Omnitrophica);species__1974739(Candidatus Omnitrophica bacterium CG02_land_8_20_14_3_00__42_8)
+	#no rank__1(root);no rank__131567(cellular organisms);superkingdom__2759(Eukaryota);kingdom__33090(Viridiplantae);phylum__35493(Streptophyta);subphylum__131221(Streptophytina);no rank__3193(Embryophyta);no rank__58023(Tracheophyta);no rank__78536(Euphyllophyta);no rank__58024(Spermatophyta);no rank__3398(Magnoliophyta);no rank__1437183(Mesangiospermae);no rank__71240(eudicotyledons);no rank__91827(Gunneridae);no rank__1437201(Pentapetalae);order__3524(Caryophyllales);family__1804623(Chenopodiaceae);subfamily__1316646(Salicornioideae);genus__46104(Salicornia);subgenus__2116532(Salicornia subg. Salicornia);species__447744(Salicornia aff. perennans Wucherer 3a;09)
 	my @taxa_peices = split /;/, $fulltaxainfo;
-	
+
 	my (%level_taxa, @temp_s_info);
-	
+
 	for my $p (@taxa_peices){
 		$p =~ /no rank/ && next;
 		my @temp_line = split /__/, $p;
+		unless($temp_line[1] =~ /\d+\((.*?)\)$/){
+			print STDERR "$p contains special characters \n";
+		}
 		my $t = $1 if $temp_line[1] =~ /\d+\((.*?)\)$/;
 		$level_taxa{$temp_line[0]} = $t;
 	}
-	
+
 	for my $i (0..$#target_level){
 		my $temp_taxa = $level_taxa{$target_level[$i]} ? "$brief_level[$i]" . "__" . $level_taxa{$target_level[$i]} : "$brief_level[$i]" . "__Unclassified";
 		push @temp_s_info, $temp_taxa;
@@ -127,14 +128,9 @@ sub extract_taxa_info{
 	}else{
 		@removed_unclassified_info = @temp_s_info[0..$#target_level-7];
 	}
-			
+
 	my $scientific_info = join(";", @removed_unclassified_info);
-	
+
 	return $scientific_info;
-	
+
 }
-
-
-
-
-
